cff1903d49462efa9b77c44d7c0b115ea77bfb50
Zibran Zarif <zzamio1112@gmail.com>
2024-04-08 22:06:13 +0600

    Initial Commit

---
+++ companies/urls.py
@@ -0,0 +1,27 @@
+BASE_URL = "https://companiesmarketcap.com" # base url
+
+### tech enpoints
+TECH = f"{BASE_URL}/tech"
+TECH_MARKET_CAP_ENDPOINT = f"{TECH}/largest-tech-companies-by-market-cap"
+TECH_EMPLOYEES_ENDPOINT = f"{TECH}/largest-tech-companies-by-number-of-employees"
+
+### pharma endpoints
+PHARMA = f"{BASE_URL}/pharmaceuticals"
+PHARMA_MARKET_CAP_ENDPOINT = f"{PHARMA}/largest-pharmaceutical-companies-by-market-cap"
+PHARMA_EMPLOYEES_ENDPOINT = f"{PHARMA}/largest-pharmaceutical-companies-by-number-of-employees"
+
+### real estate endpoints
+REAL_ESTATE = f"{BASE_URL}/real-estate"
+REAL_ESTATE_MARKET_CAP_ENDPOINT = f"{REAL_ESTATE}/largest-real-estate-companies-by-market-cap"
+REAL_ESTATE_EMPLOYEES_ENDPOINT = f"{REAL_ESTATE}/largest-real-estate-companies-by-number-of-employees"
+
+
+
+### finance ~1K (not compatible)
+### oil and gas 360
+### retail 300
+### insurance 250
+### food 250
+### electricity company 250
+### telecom 200
+### construction 100
